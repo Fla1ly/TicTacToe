79472a1e7c564944d2992dcbfba5ba182990b76b
Dawid Musial <musiald20@gmail.com>
2024-02-26 23:46:56 +0100

    commiting changes

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,13 +6,17 @@ using TicTacToe;
 string[] arr = { "", " ", " ", " ", " ", " ", " ", " ", " ", " " };
 bool gameRunning = true;
 string player;
-string player2;
+string player2 = string.Empty;
 Console.WriteLine("Welcome to my TicTacToe game!");
 Thread.Sleep(1000);
 Console.WriteLine("Who do you want to play as?");
 player = Console.ReadLine();
+Console.WriteLine($"You are playing as {player}");
 
-MainGame(arr, player, ref gameRunning, "O");
+
+
+MainGame(arr, player, ref gameRunning, player2);
+
 
 
 
@@ -20,25 +24,74 @@ static void MainGame(string[] arr, string player, ref bool gameRunning, string p
 {
     while (gameRunning)
     {
-        
+        bool spotTaken = false;
 
-        Console.WriteLine($"You are playing as {player}");
+        if (player == "X")
+        {
+            player2 = "O";
+        }
+        else if (player == "O")
+        {
+            player2 = "X";
+        }
+
         Console.WriteLine("Where do you want to play? 1-9");
         GameBoardPrinter.PrintBoard(arr);
         string moveInput = Console.ReadLine();
         int move = Int32.Parse(moveInput);
-
+        bool firstMoveDone = true;
         arr[move] = player;
         GameBoardPrinter.PrintBoard(arr);
-        CheckWin(arr, player, ref gameRunning);
+        // CheckWin(arr, player, player2, ref gameRunning);
 
-        Console.WriteLine($"Now is {player2}'s turn ");
-        moveInput = Console.ReadLine();
-        move = Int32.Parse(moveInput);
-        arr[move] = player2;
-        GameBoardPrinter.PrintBoard(arr);
+        while (firstMoveDone)
+        {
+            Console.WriteLine($"Now is {player2}'s turn ");
+            moveInput = Console.ReadLine();
+            move = Int32.Parse(moveInput);
+            arr[move] = player2;
+            GameBoardPrinter.PrintBoard(arr);
+
+            Console.WriteLine($"Now is {player}'s turn ");
+            moveInput = Console.ReadLine();
+            move = Int32.Parse(moveInput);
+            arr[move] = player;
+            GameBoardPrinter.PrintBoard(arr);
+
+            if (arr[move] == player || arr[move] == player2)
+            {
+                spotTaken = true;
+                Console.WriteLine("Spot already taken, choose another spot");
+
+            }
 
 
+            while (spotTaken == true)
+            {
+                moveInput = Console.ReadLine();
+                move = Int32.Parse(moveInput);
+
+                if (arr[move] == "")
+                {
+                    arr[move] = player;
+                    GameBoardPrinter.PrintBoard(arr);
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Spot is still taken");
+                }
+            }
+        }
+
+
+       
+
+        
+
+
+
+        
 
 
     }
@@ -53,7 +106,7 @@ switch (gameRunning == false)
         switch (finalChoice)
         {
             case "Y":
-                MainGame(arr, "", ref gameRunning, "O");
+                MainGame(arr, "", ref gameRunning, "");
                 break;
         }
         break;
@@ -61,53 +114,53 @@ switch (gameRunning == false)
 
 // Game logic
 
-static void CheckWin(string[] arr, string player, ref bool gameRunning)
+static void CheckWin(string[] arr, string player, string player2, ref bool gameRunning)
 {
     // Check win horizontal
 
-    if (arr[1] == player && arr[2] == player && arr[3] == player)
+    if (arr[1] == player && arr[2] == player && arr[3] == player ||
+        arr[4] == player && arr[5] == player && arr[6] == player ||
+        arr[7] == player && arr[8] == player && arr[9] == player)
     {
         Console.WriteLine($"Player {player} won!");
         gameRunning = false;
     }
-    else if (arr[4] == player && arr[5] == player && arr[6] == player)
+    else if (arr[1] == player2 && arr[2] == player2 && arr[3] == player2 ||
+             arr[4] == player2 && arr[5] == player2 && arr[6] == player2 ||
+             arr[7] == player2 && arr[8] == player2 && arr[9] == player2)
     {
-        Console.WriteLine($"Player {player} won!");
-        gameRunning = false;
-    }
-    else if (arr[7] == player && arr[8] == player && arr[9] == player)
-    {
-        Console.WriteLine($"Player {player} won!");
+        Console.WriteLine($"Player {player2} won!");
         gameRunning = false;
     }
 
     // Check win vertical
-    if (arr[1] == player && arr[4] == player && arr[7] == player)
+    if (arr[1] == player && arr[4] == player && arr[7] == player ||
+        arr[2] == player && arr[5] == player && arr[8] == player ||
+        arr[3] == player && arr[6] == player && arr[9] == player)
     {
         Console.WriteLine($"Player {player} won!");
         gameRunning = false;
     }
-    else if (arr[2] == player && arr[5] == player && arr[8] == player)
+    else if (arr[1] == player2 && arr[4] == player2 && arr[7] == player2 ||
+             arr[2] == player2 && arr[5] == player2 && arr[8] == player2 ||
+             arr[3] == player2 && arr[6] == player2 && arr[9] == player2)
     {
-        Console.WriteLine($"Player {player} won!");
-        gameRunning = false;
-    }
-    else if (arr[3] == player && arr[6] == player && arr[9] == player)
-    {
-        Console.WriteLine($"Player {player} won!");
+        Console.WriteLine($"Player {player2} won!");
         gameRunning = false;
     }
 
     // Check win diagonal
 
-    if (arr[1] == player && arr[5] == player && arr[9] == player)
+    if (arr[1] == player && arr[5] == player && arr[9] == player ||
+        arr[3] == player && arr[5] == player && arr[7] == player)
     {
         Console.WriteLine($"Player {player} won!");
         gameRunning = false;
     }
-    else if (arr[3] == player && arr[5] == player && arr[7] == player)
+    else if (arr[1] == player2 && arr[5] == player2 && arr[9] == player2 ||
+             arr[3] == player2 && arr[5] == player2 && arr[7] == player2)
     {
-        Console.WriteLine($"Player {player} won!");
+        Console.WriteLine($"Player {player2} won!");
         gameRunning = false;
     }
 }
